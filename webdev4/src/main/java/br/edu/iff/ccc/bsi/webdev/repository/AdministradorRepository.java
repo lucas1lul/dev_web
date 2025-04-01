d6c257045bd945673df3a0c0664a38ee0eba5dcd
Lucas Corrado <44157457+lucas1lul@users.noreply.github.com>
2025-03-31 22:48:48 -0300

    Delete webdev4 directory

--- webdev4/src/main/java/br/edu/iff/ccc/bsi/webdev/repository/AdministradorRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.iff.ccc.bsi.webdev.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import br.edu.iff.ccc.bsi.webdev.entities.Administrador;
-
-@Repository
-public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
-}
