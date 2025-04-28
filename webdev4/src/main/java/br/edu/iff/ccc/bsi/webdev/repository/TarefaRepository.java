8058361238c9cb490273bc13c7a846e9920eedeb
Lucas Corrado <44157457+lucas1lul@users.noreply.github.com>
2025-04-28 19:27:14 -0300

    Delete webdev4 directory

--- webdev4/src/main/java/br/edu/iff/ccc/bsi/webdev/repository/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.iff.ccc.bsi.webdev.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import br.edu.iff.ccc.bsi.webdev.entities.Tarefa;
-
-@Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
